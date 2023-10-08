6de5aea798151da94adbedc75d0574c33d496d53
Norbert Sapi <ncs.sapi@gmail.com>
2023-10-08 23:42:57 +0100

    update and clean SearchBar

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -1,25 +1,24 @@
 import React, { useState } from "react";
-import TrackList from './TrackList';
 import './magnifyingGlass.png';
 import styles from './SearchBar.module.css';
 
-function SearchBar() {
+function SearchBar(props) {
     const [search, setSearch] = useState('');
 
+    const searching = () => {
+        props.onSearch(search);
+    }
+
     return (
-        <div className={styles.SearchBar}>
-            {/*take out the "SEARCH BAR" title, that one is for just the visualisation for the production*/}
-            <h3 className="title">SEARCH BAR</h3>
-                <input 
-                    type="text" 
-                    placeholder=  "Search for a Song, Artist or Album" 
-                    value={search}
-                    onChange={(e) => setSearch(e.target.value)}
-                    />
-            {/* a track array passed down to List */}
-            {/*<List track={track}/>*/}
-            <button className={styles.SearchButton}>SEARCH</button>
-        </div>
+        <form className={styles.SearchBar}>
+            <input 
+                type="text" 
+                placeholder=  "Search for a Song, Artist or Album" 
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+            />
+            <button className={styles.SearchButton} type="submit" onClick={searching}>SEARCH</button>
+        </form>
     )
 };
 
