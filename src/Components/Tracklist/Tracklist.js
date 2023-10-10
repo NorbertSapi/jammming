3db62abf3ec550b94fabf85ba0094915d836e520
Norbert Sapi <ncs.sapi@gmail.com>
2023-10-10 09:58:04 +0100

    add TrackList js & css

---
+++ src/Components/Tracklist/Tracklist.js
@@ -0,0 +1,23 @@
+import React from "react";
+import styles from './Tracklist.module.css';
+import Tracks from '../Track/Track';
+
+function TrackList(props) {
+    return (
+        <div className={styles.Tracklist}>
+            {props.tracks.map((track)=> {
+                return (
+                    <Tracks
+                        onClick={props.onClick}
+                        track={track}
+                        key={track.id}
+                        inPlaylist = {props.inPlaylist}
+                        btnAction = {props.btnAction}
+                    />
+                );
+            })}
+        </div>
+    );
+}
+
+export default TrackList;
